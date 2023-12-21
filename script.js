7f68242ce5789e24c5a5916eede0f52398c4957c
Razen <09876sp54321@gmail.com>
2023-12-22 00:48:30 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,142 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    push,
+    onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+const appSettings = {
+    databaseURL:
+        "https://not-so-secret-diary-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+
+//Toggle between dark and light mode
+const modes = document.getElementById("modeIcon");
+modes.addEventListener("click", function () {
+    const styleSheet = document.getElementById("stylesheet");
+    if (styleSheet.getAttribute("href") === "dark-style.css") {
+        styleSheet.setAttribute("href", "light-style.css");
+        localStorage.setItem("theme", "light");
+    } else {
+        styleSheet.setAttribute("href", "dark-style.css");
+        localStorage.setItem("theme", "dark");
+    };
+});
+
+
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const saveMessageToDB = ref(database, "secretMessage");
+
+// HTML elements
+const messageEl = document.getElementById("message-el");
+const enterEl = document.getElementById("enter-el");
+const containerEl = document.getElementById("container-el");
+const infoEl = document.getElementById("info");
+const closeEl = document.getElementById("close")
+
+//info tab
+
+// Open Popup Function
+/* infoEl.addEventListener("click", function(){
+    const popupContainer = document.getElementById("popupContainer");
+    popupContainer.style.display = "flex";
+})
+ */
+// Close Popup Function
+/* closeEl.addEventListener("click", function(){
+    const popupContainer = document.getElementById("popupContainer");
+    popupContainer.style.display = "none";
+})
+ */
+
+// Add event listener to the Close button
+/* document.getElementById("closeBtn").addEventListener("click", closePopup); */
+
+//Function for Message Timing
+function formattedMsgTime() {
+    const now = new Date();
+    const options = {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false // Use 24-hour format
+    };
+
+    return now.toLocaleString('en-US', options).replace(',', ''); // Remove comma after the year
+}
+
+//Function to add elements to li
+
+function appendMessagesToLiEl() {
+    const msgValue = messageEl.value;
+    if (msgValue === "") {
+        alert("Enter a valid message.");
+    } else {
+        let liEl = document.createElement("li");
+        let dateSpan = document.createElement("span");
+        dateSpan.className = "message-date";
+        dateSpan.textContent = formattedMsgTime();
+        liEl.appendChild(dateSpan);
+        liEl.appendChild(document.createElement("br"));
+        liEl.appendChild(document.createTextNode(msgValue));
+        containerEl.appendChild(liEl);
+    }
+}
+
+//Adding message to list function
+enterEl.addEventListener("click", function () {
+    appendMessagesToLiEl();
+
+});
+
+//Adding input Field to database
+
+enterEl.addEventListener("click", function () {
+    containerEl.textContent = "";
+    let msgValue = messageEl.value;
+    const combinedMessage = {
+        timestamp: formattedMsgTime(),
+        content: msgValue
+    };
+
+    push(saveMessageToDB, combinedMessage)
+    messageEl.value = "";
+});
+
+
+
+//Reloading the existing messages from database
+
+onValue(saveMessageToDB, function (snapshot) {
+    messageEl.textContent = "";
+    if (snapshot.exists()) {
+        let messagesInDB = Object.values(snapshot.val());
+        for (let i = 0; i < messagesInDB.length; i++) {
+            let messageValue = messagesInDB[i];
+            let messageContent = messageValue.content;
+            let messageTimeStamp = messageValue.timestamp;
+            let liEl = document.createElement("li");
+            let dateSpan = document.createElement("span");
+            dateSpan.className = "message-date";
+            dateSpan.textContent = messageTimeStamp;
+            liEl.appendChild(dateSpan);
+            liEl.appendChild(document.createElement("br"));
+            liEl.appendChild(document.createTextNode(messageContent));
+            containerEl.appendChild(liEl);
+        }
+    } else {
+        containerEl.innerHTML = "No messages to be displayet yet..."
+    }
+}
+);
+
+
+
+
+
